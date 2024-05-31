2a95f720117b441ca59f33acfb4f11254b62bdcd
jaspreet singh <preetsansoya008@gmail.com>
2024-05-31 22:56:19 +0530

    up

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 }
 
 const Container = styled.div`
-margin-left: 66px;
+margin-left: 56px;
 
 img{
   width: 100px;
@@ -79,7 +79,7 @@ button{
 }
 
 .last{
-  margin-top: 220px;
+  margin-top: 250px;
 }
 
 
